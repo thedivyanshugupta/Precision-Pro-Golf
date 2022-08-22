b2eb02a74e503614e123a9bad0d8cc323796a6f5
Divyanshu <Divyanshugupta17@gmail.com>
2022-08-22 13:15:36 +0530

    Launch Screen completed

---
+++ GolferApplication/GolferApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolferApplicationApp.swift
+//  GolferApplication
+//
+//  Created by Roro on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GolferApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
